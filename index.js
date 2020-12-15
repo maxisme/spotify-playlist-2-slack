126dcb6a95bf11a3c7b765044da9ad0e627593b4
Maximilian Mitchell <max@maxis.me>
2020-12-15 21:01:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ var slacker = slack.extend({
 });
 
 function post(list_name, list_url, href, added_by, trackname, artists) {
-  var text = href + '\n<'+list_url+'|*' + trackname+'* by '+artists[0].name+'>';
+  var text = '<'+href+'|*' + trackname+'* by '+artists[0].name+'>';
   console.log(text);
   slacker({text: text});
 }
